199a45224b809692a8d4d75dcfa051027257a962
Len Budney <Len.Budney@grantstreet.com>
2019-05-16 17:43:56 -0400

    Changed Apache license to GPL 3.

--- home-energy-logger.groovy
+++ home-energy-logger.groovy
@@ -3,14 +3,26 @@
  *
  *  Copyright 2019 Leonard Budney
  *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software  * Foundation, either version 3 of the License,
+ * or (at your option) any later version, along with the following
+ * terms:
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ * 1. You may convey a work based on this program in accordance
+ * with section 5, provided that you retain the above notices.
  *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
+ * 2. You may convey verbatim copies of this program code as you
+ * receive it, in any medium, provided that you retain the above
+ * notices.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS    * FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  */
 definition(
